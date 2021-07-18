3f6a2b6c4e1a250a94424e636d3e9b893f056ea4
kenhua-l <l_kenhua93@hotmail.com>
2021-07-18 12:24:44 +0800

    todo list on App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/bootstrap.min.css';
+import './css/all.min.css';
+import './css/todolist.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div class="container">
+        <h1>TO-DO LIST</h1>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="What to you want to do?" aria-label="Recipient's username" aria-describedby="button-addon2" />
+          <button class="btn btn-outline-secondary" type="button" id="button-addon2">Add</button>
+        </div>
+        <div class="todo-list-container">
+          <ul>
+            <li>
+              <p>
+                Send an email update to the team: 9am today
+              </p>
+              <div class="list-action">
+                <button class=""><span class="fas fa-edit"></span></button>
+                <button class=""><span class="fas fa-times"></span></button>
+              </div>
+            </li>
+            <li>
+              <p>
+                Call the design agency to finalize mockups: 1:00pm today
+              </p>
+              <div class="list-action">
+                <button class=""><span class="fas fa-edit"></span></button>
+                <button class=""><span class="fas fa-times"></span></button>
+              </div>
+            </li>
+            <li>
+              <p>
+                Touch base with recruiters about new role: Tuesday
+              </p>
+              <div class="list-action">
+                <button class=""><span class="fas fa-edit"></span></button>
+                <button class=""><span class="fas fa-times"></span></button>
+              </div>
+            </li>
+            <li>
+              <p>
+                Meet with the engineering team: Thursday
+              </p>
+              <div class="list-action">
+                <button class=""><span class="fas fa-edit"></span></button>
+                <button class=""><span class="fas fa-times"></span></button>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 }
 
